1a74adb8c9a3ce88b989ac9ea074c08c3f9322c6
Marcelo Tozzi <marcelotozzidelima@gmail.com>
2018-01-23 19:59:40 -0200

    MainWindow Class

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const path = require('path');
 const TimerTray = require('./app/timer_tray');
+const MainWindow = require('./app/main_window');
 
 const { app, BrowserWindow } = electron;
 
@@ -9,17 +10,7 @@ let tray;
 
 app.on('ready', () => {
   app.dock.hide();
-  mainWindow = new BrowserWindow({
-    height:500,
-    width: 300,
-    frame: false,
-    resizable: false,
-    show: false
-  });
-  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-  mainWindow.on('blur', () => {
-    mainWindow.hide();
-  });
+  mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
