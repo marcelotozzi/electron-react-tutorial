59b077cdbda1101fc507838a5c5c95f98d6a4342
Marcelo Tozzi <marcelotozzidelima@gmail.com>
2018-01-23 21:00:17 -0200

    Starting up Electron with Webpack

--- boilerplates/convert/index.js
+++ boilerplates/convert/index.js
@@ -2,10 +2,15 @@ const electron = require('electron');
 
 const { app, BrowserWindow } = electron;
 
-app.app.on('ready', () => {
-  new BrowserWindow({
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
     webPreferences: { backgroundThrottling: false }
   });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+
 });
