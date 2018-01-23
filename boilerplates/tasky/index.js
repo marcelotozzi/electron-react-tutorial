ad012e84b7e1f22685103a1b0fa6285d94645ec8
Marcelo Tozzi <marcelotozzidelima@gmail.com>
2018-01-23 15:03:11 -0200

    Detecting Visibilty

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -20,6 +20,10 @@ app.on('ready', () => {
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
   tray = new Tray(iconPath);
   tray.on('click', () => {
-    mainWindow.show();
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();      
+    }else{
+      mainWindow.show();
+    }
   });
 });
